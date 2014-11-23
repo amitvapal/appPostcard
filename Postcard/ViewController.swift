a3f310858fc8d631cde3b5e53db1e34348f2c7b1
Amitva Pal <amitvapal@Rajendras-MBP.Home>
2014-11-23 14:27:46 -0500

    second rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    
     @IBOutlet weak var mailButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +34,22 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.blueColor()
+      
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+    //  nameLabel.backgroundColor = UIColor.blueColor()
+        nameLabel.textColor = UIColor.blueColor()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
-        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
-    }
+      mailButton.setTitle("Mail Sent" , forState: UIControlState.Normal)
+        
+        }
 
 }
 
